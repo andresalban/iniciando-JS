343931c64eeda4c0e9e110a859540d15be79ec11
Andres Alban <andresalban@unicauca.edu.co>
2025-01-31 13:38:42 -0500

    aula 2 terminada

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+//varaibles
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un numero por favor:");
+let numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor:");
 
 console.log(numeroUsuario);
-
+/*
+Este codigo realiza 
+la comparacion 
+*/
 if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el numero');
+    //acertamos ,fue verdadera la condicion
+    alert(`Acertaste,el número es: ${numeroUsuario}`);
+}else{
+    //la condicion no se cumplio
+alert("Lo siento,no acertaste en número")
 }
