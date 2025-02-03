2f7ab0c2dec59a633d3d5c32cdbccb9f4a6933d2
Andres Alban <andresalban@unicauca.edu.co>
2025-02-03 09:21:22 -0500

    aula 3 terminada

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 //varaibles
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor:");
+let numeroUsuario = 0;
+let intentos = 1;
+let palabraVeces= 'vez'
+while (numeroUsuario != numeroSecreto) {
+  numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor:");
 
-console.log(numeroUsuario);
-/*
-Este codigo realiza 
-la comparacion 
-*/
-if (numeroUsuario == numeroSecreto) {
+  console.log(numeroUsuario);
+  /*
+    Este codigo realiza la comparacion 
+    */
+  if (numeroUsuario == numeroSecreto) {
     //acertamos ,fue verdadera la condicion
-    alert(`Acertaste,el número es: ${numeroUsuario}`);
-}else{
+    alert(`Acertaste,el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraVeces}`);
+  } else {
+    if (numeroUsuario > numeroSecreto) {
+      alert("El numero secreto es menor");
+    } else {
+      alert("el numero secreto es mayor");
+    }
+    //incrementamos el contador cuando no acierta
+    intentos = intentos + 1;
+    palabraVeces='veces'
     //la condicion no se cumplio
-alert("Lo siento,no acertaste en número")
+    //alert("Lo siento,no acertaste en número")
+  }
 }
